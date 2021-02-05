491cf929428694e68c4774d099a91e67816ba157
HAKAN BIYIK <hakanbiyik91@gmail.com>
2021-02-05 11:21:08 +0100

    added words and clues

--- gameplay.rb
+++ gameplay.rb
@@ -1,9 +1,28 @@
 class Hangman
 
   def initialize
+    @letters = ("a".."z").to_a
+    @word = words.sample
+  end
+
+  def words
+    [
+      "cricket", "A game played by gentlemen"
+      "jogging", "We are not walking..."
+      "celebrate", "Remembering special moments"
+      "continent", "There are 7 of these"
+      "exotic", "Not from around here..."
+    ]
   end
 
   def begin
     #ask user to start with a letter
+    puts "New game started... your clue is #{ @word.first }"
+    guess = gets.chomp
+
+    puts "You guessed #{guess}"
   end
+
 end
+
+game = Hangman.new
